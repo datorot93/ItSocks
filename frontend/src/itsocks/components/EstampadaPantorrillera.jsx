643325cac4876d8f7335019af9f7aa34ea85e39f
datorot@outlook.com <datorot@datorotoutlookcoms-MacBook-Pro.local>
2023-05-20 21:46:15 -0500

    Most routes created

---
+++ frontend/src/itsocks/components/EstampadaPantorrillera.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const EstampadaPantorrillera = () => {
+  return (
+    <div>EstampadaPantorrillera</div>
+  )
+}
---
+++ frontend/src/itsocks/components/ProductDescription.jsx
@@ -0,0 +1,129 @@
+import React, { useEffect, useMemo, useState } from 'react'
+
+// Estilos
+import styles from '../../ui/styles/ProductDescription.module.css';
+import { Link, useParams } from 'react-router-dom';
+import { getProductsByName } from '../helpers/getProductByName';
+import { getProductsByPartOfName } from '../helpers/getProductsByPartOfName';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+// Imágenes
+import carrito from '../../../public/assets/producto/carrito.svg';
+import corazon from '../../../public/assets/producto/corazon.svg';
+import camion from '../../../public/assets/producto/camion.svg';
+import reloj from '../../../public/assets/producto/reloj.svg';
+
+export const ProductDescription = () => {
+
+    const [selectedIndex, setSelectedIdenx] = useState(0);
+    
+    const { nombre, ...rest} =useParams();
+    const [ producto ]= getProductsByName( nombre );
+    const initialState = producto.images.slice(1, producto.images.length);
+    const [ otherPhotos, setOtherPhotos ] = useState(initialState);
+    const similares = getProductsByPartOfName( producto.nombre );
+
+    const [ cantProducts, setCantProducts ] = useState(0);
+    
+
+    const next = () => {
+        const condition = selectedIndex < (producto.images.length -1)
+        const nextIndex = condition ? selectedIndex + 1 : 0;
+        setSelectedIdenx(nextIndex);
+        setOtherPhotos( producto.images.filter( image => image != producto.images[nextIndex]));
+    }
+
+    
+
+    const previus = () => {
+        const condition = selectedIndex > 0
+        const nextIndex = condition ? selectedIndex - 1 : (producto.images.length -1);
+        setSelectedIdenx(nextIndex);
+        setOtherPhotos( producto.images.filter( image => image != producto.images[nextIndex]));
+    }
+    
+
+  return (
+    <div className={ styles.main }>
+        <div className={ styles.description_container }>
+
+            <div className={ styles.image_container }>
+                <div className={ styles.principal_image }>
+                    <button  className={ `${styles.arrow_button} ${ styles.left}`} onClick={ previus }>{'<'}</button>
+                    <LazyLoadImage src={ producto.images[selectedIndex] } alt={ producto.nombre } />
+                    <button  className={ `${styles.arrow_button} ${ styles.right }`} onClick={ next }>{'>'}</button>
+                </div>
+                <div className={ styles.similares }>
+                    {otherPhotos.map( (image, index) => (
+                        <LazyLoadImage src={ image } alt={ producto.nombre } key={ index } />
+                    ))}
+                </div>
+            </div>
+
+            <div className={ styles.description }>
+                <h2>{ nombre }</h2>
+                <div className={ styles.precio }>
+                    <p>{ `$ ${producto.precio}` }</p>
+                </div>
+                <div className={ styles.description_p }>
+                    <p>{ producto.descripcion }</p>
+                </div>
+                {/* <div className={ styles.colores }>
+                    <span>Colores: </span>
+                    {
+                        all_products.map( producto => (
+                            <div className={ styles.single_color } key={ producto.id }></div>
+                        ))
+                    }
+                </div> */}
+                <div className={ styles.acciones }>
+                    <div className={ styles.carrito }>
+                        <img src={ carrito } alt= "Carrito de compras" />
+                        <span>Agregar a carrito</span>
+                    </div>
+                    <div className={ styles.corazon }>
+                        <img src={ corazon } alt= "Corazon" />
+                        <span>Añadir a mi lista</span>
+                    </div>
+                </div>
+
+                <div className={ styles.comprar }>
+                    <div className={ styles.conteo }>
+                        <button onClick={ () => setCantProducts( cantProducts + 1)}>+</button>
+                        <span>{ cantProducts }</span>
+                        <button onClick={ () => setCantProducts( cantProducts - 1)}>-</button>
+                    </div>
+                    <button className={ styles.boton_comprar}>¡¡COMPRAR AHORA!!</button>
+
+                </div>
+
+                <div className={ styles.informacion_adicional }>
+                    <img src={ camion } alt="Envíos" />
+                    <p>Lleva <span>$200.000</span> más y el envío te sale gratis</p>                
+                </div>
+
+                <div className={ styles.tiempo_estimado }>
+                    <img src={ reloj } alt="Reloj" />
+                    <p>Tiempo estimado de envio entre lunes 24 abril and martes 25 abril.</p>                
+                </div>
+            </div>
+
+        </div>
+
+        <div className={ styles.productos_recomendados }>
+            <h2>Productos recomendados 🔥</h2>
+            <div className={styles.recommended_images}>
+                
+                { similares.map( producto => (
+                    <LazyLoadImage 
+                        key={ producto.id} 
+                        src={ producto.images } 
+                        alt={ producto.nombre }
+
+                    />
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
