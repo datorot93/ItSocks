dbdebd33632e2e182c2393059728fc87af427623
datorot@outlook.com <datorot@datorotoutlookcoms-MacBook-Pro.local>
2023-06-04 11:04:41 -0500

    agregando el crud de subcategorias al proyecto y agregando algunos gitignire para evitar subir algunos archivos innecesarios en todo el proyecto

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
 # from app.api.api_v1.routers import login, users
-from app.api.api_v1.routers import categories
+from app.api.api_v1.routers import categories, subcategories
 
 api_router = APIRouter()
 
 api_router.include_router(categories.router, prefix="/categories", tags=["category"])
+api_router.include_router(subcategories.router, prefix="/subcategories", tags=["subcategory"])
---
+++ backend/app/api/api_v1/routers/subcategories.py
@@ -0,0 +1,10 @@
+from typing import Any, List
+
+from fastapi import APIRouter, Request, Depends, HTTPException, Response
+from sqlalchemy.orm import Session
+
+from app import crud, models, schemas
+from app.api import deps
+
+router = APIRouter()
+
