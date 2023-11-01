876e1532be369d9456c3c4d4421cde6d396e3294
datorot@outlook.com <datorot@datorotoutlookcoms-MacBook-Pro.local>
2023-11-01 15:37:04 -0500

    Some trash files cleaning from de models

--- frontend/src/actions/setPack.js
+++
@@ -1,8 +0,0 @@
-
-
-export const setPack = ({ pack }) => {
-    
-    localStorage.setItem('pack', pack)
-
-    
-}
--- frontend/src/itsocks/types/types.js
+++
@@ -1,5 +0,0 @@
-
-export const types = {
-  setProducts: '[PRODUCTS] SetProducts',
-  setBreadCrumb: '[BREADCRUMBS] SetBreadCrumb'
-}
