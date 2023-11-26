e376064355750c2c58236ce9453f7ed713e9f948
Daniel Toro <datorot@outlook.com>
2023-11-26 12:56:31 -0500

    Imágenes eliminadas

--- frontend/src/itsocks/data/slider_images.js
+++
@@ -1,9 +0,0 @@
-
-
-export const images = [
-    'https://placekitten.com/300/200',
-    'https://placekitten.com/300/201',
-    'https://placekitten.com/300/202',
-    'https://placekitten.com/300/203',
-    'https://placekitten.com/300/204'
-  ];
--- frontend/src/router/PublicRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// import { Navigate, useLocation } from 'react-router-dom';
-
-export const PublicRoutes = ({ children }) => {
-
-  return (
-    children
-  )
-}
