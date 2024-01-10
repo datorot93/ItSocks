04c31660d332715f65b49e4082835edf628110c2
sebastianc <sebastian.cardenasdue@gmail.com>
2024-01-09 19:52:56 -0500

    Update landing con sliders, animaciones pendientes

--- frontend/src/itsocks/components/ScrollHorizontal.jsx
+++ frontend/src/itsocks/components/ScrollHorizontal.jsx
@@ -1,105 +1,110 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import Slider from "react-slick";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+  faCircleChevronLeft,
+  faCircleChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import styles from '../../ui/styles/ScrollHorizontal.module.css';
+import styles from "../../ui/styles/ScrollHorizontal.module.css";
 
 function ScrollHorizontal() {
-
   const masVendidos = [
     {
       id: "p1",
       src: "../../../public/assets/medias/productos/p1.png",
       description: "Esta es una media",
+      price: "10000",
     },
     {
       id: "p2",
       src: "../../../public/assets/medias/productos/p2.png",
       description: "Esta es una media",
+      price: "10000",
     },
     {
       id: "p3",
       src: "../../../public/assets/medias/productos/p3.png",
       description: "Esta es una media",
+      price: "10000",
     },
     {
       id: "p4",
       src: "../../../public/assets/medias/productos/p5.png",
       description: "Esta es una media",
+      price: "10000",
     },
   ];
 
-  function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "red", paddingLeft: 20}}
-        onClick={onClick}
-      />
-    );
-  }
+  const CustomNextArrow = (props) => (
+    <div className={styles.custom_next_arrow} onClick={props.onClick}>
+      <FontAwesomeIcon icon={faCircleChevronRight} style={{ color: "black" }} />
+    </div>
+  );
 
-  function SamplePrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "green" }}
-        onClick={onClick}
-      />
-    );
-  }
+  const CustomPrevArrow = (props) => (
+    <div className={styles.custom_prev_arrow} onClick={props.onClick}>
+      <FontAwesomeIcon icon={faCircleChevronLeft} style={{ color: "black" }} />
+    </div>
+  );
   var settings = {
     dots: true,
     infinite: false,
     speed: 500,
     arrows: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 4,
+    slidesToScroll: 4,
     initialSlide: 0,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
+    nextArrow: <CustomNextArrow />,
+    prevArrow: <CustomPrevArrow />,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 4,
+          slidesToScroll: 4,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   return (
     <Slider {...settings}>
       {masVendidos.map((image, index) => (
-        <div className={styles.img_container} key={ index }>
-          <div className={styles.img_center}>
-            <img src={image.src} alt={image.descruption} key={image.id} />
+        <div className={styles.img_container} key={index}>
+          <div className={styles.escalable}>
+            <div className={styles.img_center}>
+              <img src={image.src} alt="" key={image.id} />
+            </div>
+          </div>
+          <div>
+            <p className={styles.description_image}>{image.description}</p>
+            <p className={styles.price_image}>{image.price}</p>
           </div>
         </div>
       ))}
     </Slider>
-
   );
 }
 
